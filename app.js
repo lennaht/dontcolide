b70b040beb43e885eaf296d89fc8ea9d3be5ab78
Lennaht <33487842+lennaht@users.noreply.github.com>
2017-11-09 15:48:54 +0100

    Win detection and more

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+//get canvas and it's context
 const cv = document.querySelector('#cv');
 const cx = cv.getContext('2d');
 
+//store window scale
 const wh = window.innerHeight;
 const ww = window.innerWidth;
 
+//scale of canvas
 cv.height = 700;
 cv.width = 700;
 
@@ -14,9 +17,11 @@ class Player {
         this.y = y;
         this.speed = speed;
         this.line_positions = [];
+        this.hit = false;
     }
 
     move(key) {
+        //move blue player
         if(this.color == 'blue') {
             switch(key) {
                 case 'w':
@@ -33,6 +38,7 @@ class Player {
             }
         }
 
+        //move red player
         if(this.color == 'red') {
             switch(key) {
                 case 'ArrowUp':
@@ -49,11 +55,50 @@ class Player {
             }
         }
 
+        //save position for line and collision controle
         this.line_positions.push({x: this.x, y: this.y});
-        c.beginPath();
-        c.rect(this.x, this.y, 10, 10);
-        c.fillStyle = this.color;
-        c.fill();
+
+        //draw player
+        cx.beginPath();
+        cx.rect(this.x, this.y, 10, 10);
+        cx.fillStyle = this.color;
+        cx.fill();
     }
 
+}
+
+//initiate players
+var playerBlue = new Player('blue', Math.floor(cv.width / 10), cv.width - (Math.floor(cv.height / 2)), 3);
+var playerRed = new Player('red', Math.floor(cv.width - (cv.width / 10)), cv.width - (Math.floor(cv.height / 2)), 3);
+
+//last pressed buttons
+var buttonBlue = 'd';
+var buttonRed = 'ArrowRight'
+
+function play() {
+    //check if player hit a line and call winner, get hit -> opponent wins
+    if(playerBlue.hit === true) {
+        alert('Red won!');
+    } else if(playerRed.hit === true) {
+        alert('Blue won!');
+    } else {
+        document.addEventListener('keydown', function(e){
+            //store button pressed by blue player (w, a, s, d)
+            if(e.key == 'w' || e.key == 'a' || e.key == 's' || e.key == 'd') {
+                buttonBlue = e.key;
+            }
+
+            //store button pressed by red player (arrows)
+            if(e.key == 'ArrowUp' || e.key == 'ArrowLeft' || e.key == 'ArrowDown' || e.key == 'ArrowRight') {
+                buttonRed = e.key;
+            }
+        });
+
+        requestAnimationFrame(play);
+        cv.clearRect(0, 0, cv.width, cv.height);
+
+        playerBlue.move(buttonBlue);
+        playerRed.move(buttonRed);
+    } //end of if...else if...else
+    
 }
