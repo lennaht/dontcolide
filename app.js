0a65bc9ea8385ded68a2c466e8ccf45002951641
Lennaht <33487842+lennaht@users.noreply.github.com>
2017-11-09 15:21:19 +0100

    Edited Player class

--- app.js
+++ app.js
@@ -8,13 +8,52 @@ cv.height = 700;
 cv.width = 700;
 
 class Player {
-    constructor(color, x, y) {
+    constructor(color, x, y, speed) {
         this.color = color;
         this.x = x;
         this.y = y;
+        this.speed = speed;
+        this.line_positions = [];
     }
 
-    move(event) {
-        
+    move(key) {
+        if(this.color == 'blue') {
+            switch(key) {
+                case 'w':
+                    this.y += speed;
+                    break;
+                case 'a':
+                    this.x -= speed;
+                    break;
+                case 's':
+                    this.y -= speed;
+                    break;
+                case 'd':
+                    this.x += speed;
+            }
+        }
+
+        if(this.color == 'red') {
+            switch(key) {
+                case 'ArrowUp':
+                    this.y += speed;
+                    break;
+                case 'ArrowLeft':
+                    this.x -= speed;
+                    break;
+                case 'ArrowDown':
+                    this.y -= speed;
+                    break;
+                case 'ArrowRight':
+                    this.x += speed;
+            }
+        }
+
+        this.line_positions.push({x: this.x, y: this.y});
+        c.beginPath();
+        c.rect(this.x, this.y, 10, 10);
+        c.fillStyle = this.color;
+        c.fill();
     }
+
 }
